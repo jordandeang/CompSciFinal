f0d1b118222ff3b31b04222ea319bda07ffe909c
Jordan Deang <your_email@example.com>
2014-06-10 23:50:57 -0400

    Test
    2

--- src/com/pewpy/pewp/level/tile/Tile.java
+++ src/com/pewpy/pewp/level/tile/Tile.java
@@ -30,7 +30,7 @@ public class Tile {
 	public static Tile riverhor = new Tile(Sprite.riverhor);
 	public static Tile log = new Tile(Sprite.log);
 
-	//all i wanted was a 10dollas
+	//all i wa
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
 	}
