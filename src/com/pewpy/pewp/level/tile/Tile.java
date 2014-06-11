c97de2495a73bd57e3851e74d330e6e93609f26e
mantism <mikael@mikaels-imac.home>
2014-06-10 23:41:02 -0400

    yolo

--- src/com/pewpy/pewp/level/tile/Tile.java
+++ src/com/pewpy/pewp/level/tile/Tile.java
@@ -29,7 +29,7 @@ public class Tile {
 	public static Tile riverturn = new Tile(Sprite.riverturn);
 	public static Tile riverhor = new Tile(Sprite.riverhor);
 	public static Tile log = new Tile(Sprite.log);
-	//testickles
+	//testicklesno
 	
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
