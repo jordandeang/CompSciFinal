598d98add15a5a7852f1eb37f4fbcef2ab53bfab
mantism <temp@Erinis-MacBook-Pro.local>
2014-06-12 08:08:50 -0400

    SolidTiles

--- src/com/pewpy/pewp/level/tile/TreeTile.java
+++ src/com/pewpy/pewp/level/tile/TreeTile.java
@@ -3,12 +3,13 @@ package com.pewpy.pewp.level.tile;
 import com.pewpy.pewp.graphics.Screen;
 import com.pewpy.pewp.graphics.Sprite;
 
-public class GrassTile extends Tile {
+public class TreeTile extends Tile {
 
-	public GrassTile(Sprite sprite) {
+	public TreeTile(Sprite sprite) {
 		super(sprite);
+		// TODO Auto-generated constructor stub
 	}
-
+	
 	public void render(int x, int y, Screen screen) {
 		screen.renderTile(x << 4, y << 4, this);
 	}
