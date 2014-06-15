af7c66355a835152627b0ad77ac9fd10e789775b
tylercmartin <tylercmartin@yahoo.com>
2014-06-15 01:57:27 -0400

    Changed map like crazyy
    Game.class
    test.png

--- src/com/pewpy/pewp/Game.java
+++ src/com/pewpy/pewp/Game.java
@@ -42,7 +42,7 @@ public class Game extends Canvas implements Runnable {
 		frame = new JFrame();
 		screen = new Screen(width, height);
 		key = new Keyboard();
-		TileCoord playerSpawn = new TileCoord(16,3);
+		TileCoord playerSpawn = new TileCoord(3,3);
 		level = new LevelOne("/textures/test.png");
 		player = new Player(playerSpawn.x(), playerSpawn.y(),key);
 		player.init(level);
